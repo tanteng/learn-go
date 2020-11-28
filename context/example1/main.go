7910e05225d748a111171eecfc439e2cf6d95a5f
tanteng <tanteng@gmail.com>
2020-11-29 02:05:27 +0800

    context 包的使用

--- context/example1/main.go
+++ context/example1/main.go
@@ -9,9 +9,10 @@ import (
 
 var wg sync.WaitGroup
 
-// 定义一个带超时时间的 ctx，传入每个 goroutine 第一个参数，实现超时退出协程的效果
+// main 设置一个 3s 超时的 ctx，传给每个 goroutine 第一个参数，每个 goroutine 又发起协程处理任务，
+// 假设需要 5s，通过监听 ctx.Done 判断是否超时退出
 func main() {
-	ctx, cancel := context.WithTimeout(context.Background(), 4*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
 	wg.Add(2)
@@ -25,16 +26,15 @@ func main() {
 func watch(ctx context.Context, flag int) {
 	defer wg.Done()
 
-	fmt.Printf("doing something flag:%d\n", flag)
-	time.Sleep(5 * time.Second)
+	go func() {
+		fmt.Printf("doing something flag:%d\n", flag)
+		time.Sleep(5 * time.Second)
+		fmt.Println("finished flag:", flag)
+	}()
 
-	for {
-		select {
-		case <-ctx.Done():
-			fmt.Printf("watch %d %s\n", flag, ctx.Err())
-			return
-		case <-time.After(2 * time.Second):
-			fmt.Println("2 second")
-		}
+	select {
+	case <-ctx.Done():
+		fmt.Printf("watch %d %s\n", flag, ctx.Err())
+		return
 	}
 }
