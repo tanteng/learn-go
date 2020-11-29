6d5cc0a2ec5ddff561675a27798248c4a7a075ae
tanteng <tanteng@gmail.com>
2020-11-29 20:32:25 +0800

    context 包的使用

--- context/example1/main.go
+++ context/example1/main.go
@@ -12,7 +12,7 @@ var wg sync.WaitGroup
 // main 设置一个 3s 超时的 ctx，传给每个 goroutine 第一个参数，每个 goroutine 又发起协程处理任务，
 // 假设需要 5s，通过监听 ctx.Done 判断是否超时退出
 func main() {
-	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 4*time.Second)
 	defer cancel()
 
 	wg.Add(2)
@@ -20,21 +20,28 @@ func main() {
 	go watch(ctx, 2)
 	wg.Wait()
 
-	fmt.Println("finished")
+	fmt.Println("All finished")
 }
 
 func watch(ctx context.Context, flag int) {
 	defer wg.Done()
 
-	go func() {
-		fmt.Printf("doing something flag:%d\n", flag)
-		time.Sleep(5 * time.Second)
-		fmt.Println("finished flag:", flag)
-	}()
+	done := make(chan bool)
+	go func(done chan<- bool) {
+		fmt.Printf("[flag:%d]doing something\n", flag)
+		time.Sleep(6 * time.Second)
+		done <- true
+	}(done)
 
 	select {
 	case <-ctx.Done():
-		fmt.Printf("watch %d %s\n", flag, ctx.Err())
+		fmt.Printf("[flag:%d]ctx done,err: %s\n", flag, ctx.Err())
+		return
+	case <-time.After(8 * time.Second):
+		fmt.Printf("[flag:%d]timeout\n", flag)
+		return
+	case <-done:
+		fmt.Printf("[flag:%d]finished\n", flag)
 		return
 	}
 }
