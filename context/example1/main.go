711cfdc59afeee9016cef44f0b18ad4cf44ec4be
tanteng <tanteng@gmail.com>
2020-12-14 00:22:05 +0800

    Revert "context 包的使用"
    This reverts commit fc8d1a65942ff18d9fc0409f94fb6afe74fc78d9.

--- context/example1/main.go
+++ context/example1/main.go
@@ -9,18 +9,15 @@ import (
 
 var wg sync.WaitGroup
 
-// main 设置一个带有超时时间的 ctx，子协程通过监听 ctx.Done 判断是否超时
+// main 设置一个 3s 超时的 ctx，传给每个 goroutine 第一个参数，每个 goroutine 又发起协程处理任务，
+// 假设需要 5s，通过监听 ctx.Done 判断是否超时退出
 func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), 4*time.Second)
 	defer cancel()
 
 	wg.Add(2)
-
-	go func() {
-		go watch(ctx, 1)
-		go watch(ctx, 2)
-	}()
-
+	go watch(ctx, 1)
+	go watch(ctx, 2)
 	wg.Wait()
 
 	fmt.Println("All finished")
@@ -32,13 +29,16 @@ func watch(ctx context.Context, flag int) {
 	done := make(chan bool)
 	go func(done chan<- bool) {
 		fmt.Printf("[flag:%d]doing something\n", flag)
-		time.Sleep(2 * time.Second)
+		time.Sleep(6 * time.Second)
 		done <- true
 	}(done)
 
 	select {
 	case <-ctx.Done():
 		fmt.Printf("[flag:%d]ctx done,err: %s\n", flag, ctx.Err())
+		return
+	case <-time.After(8 * time.Second):
+		fmt.Printf("[flag:%d]timeout\n", flag)
 		return
 	case <-done:
 		fmt.Printf("[flag:%d]finished\n", flag)
